e9f370572bda881d224d767f818d0352071f0b2f
Cheat3r <49972382+guga-07@users.noreply.github.com>
2020-07-20 11:05:18 +0400

    Update Kali.sh

--- Kali.sh
+++ Kali.sh
@@ -2,6 +2,8 @@
 echo -e "\e[36mCodded By:Cheat3r\e[0m"
 apt update -y
 apt upgrade -y
+apt install lolcat -y
+echo Follow |lolcat -a -d 50
 pkg install wget -y
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
