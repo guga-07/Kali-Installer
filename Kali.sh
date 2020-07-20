beb91b4858c76d453005a54d305715aeb977797e
Cheat3r <49972382+guga-07@users.noreply.github.com>
2020-07-20 10:38:16 +0400

    Create Kali.sh

---
+++ Kali.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\e[36mCodded By:Cheat3r\e[0m"
+apt update -y
+apt upgrade -y
+pkg install wget -y
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
